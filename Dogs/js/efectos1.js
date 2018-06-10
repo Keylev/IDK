2ae1b178f99b5f48d56a9348bcc5485b129737be
Keylev <40141511+Keylev@users.noreply.github.com>
2018-06-10 06:12:16 -0600

    Add files via upload

---
+++ Dogs/js/efectos1.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+
+
+ //efecto menu
+  $('.menu a').each(function(index, elemento){
+
+    $(this).css({
+      'top' :'-200px'
+    });
+
+    $(this).animate({
+      top:'0'
+
+    }, 2000 + (index * 500));
+
+  });
+
+  //efecto header
+
+
+  if($ (window).width() > 800){
+    $('header .textos').css({
+      opacity: 0,
+      marginTop:0
+    });
+    $('header .textos').animate({
+      opacity: 1,
+      marginTop:'-52px'
+    }, 1500);
+  }
+
+  //scroll elementos menu
+
+
+  var galeria = $('#galeria').offset().top,
+    menu = $('#platillos').offset().top,
+    Ubicacion = $('#Ubicacion').offset().top;
+
+  $('#btn-acerca-de').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: acercaDe -100
+    }, 500);
+  });
+  $('#btn-menu').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: menu
+    }, 500);
+  });
+  $('#btn-galeria').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: galeria
+    }, 500);
+  });
+  $('#btn-ubicacion').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: Ubicacion
+    }, 500);
+  });
+});
